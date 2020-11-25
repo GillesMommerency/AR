fad0c44d9225b7aaa445f2b2aef065b610084748
GillesMommerency <gilles.mommerency@hotmail.com>
2020-11-25 13:27:35 +0100

    Working switch (Animation still needed)

--- Assets/Simple Animated 3D Buttons/CoreLogic/ButtonLogic.cs
+++ Assets/Simple Animated 3D Buttons/CoreLogic/ButtonLogic.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-public class ButtonLogic : MonoBehaviour {
+public class ButtonLogic : MonoBehaviour
+{
 
 	[Header("Target Method")]
 	[SerializeField]
@@ -29,33 +30,57 @@ public class ButtonLogic : MonoBehaviour {
 	private float currentDestination = 0;
 	private Vector3 startPos;
 	static HttpClient client = new HttpClient();
+	[SerializeField]
+	public static bool aanUit { get; set; }
+
 
 
 
 	// Use this for initialization
-	void Start () 
+	void Start()
 	{
-		if(buttonMeshObject != null)
+		
+		if (buttonMeshObject != null)
 		{
 			startPos = buttonMeshObject.transform.localPosition;
 		}
-		
+
 		//gameObject.tag = "button3D"; //This is required if the tag based solution is selected. Can be disabled once it has been set, it's simply just to ensure that the user (you), is aware of this tag's importance.
 
 	}
-	
+
 
 
 
 	public void OnClick()
 	{
+
 		MethodsToCallMouseDown.Invoke();
-		var mycolor = gameObject.transform.GetChild(1).gameObject.GetComponent<Renderer>().material.color;
-		Debug.Log("Hello:" + ColorUtility.ToHtmlStringRGB(mycolor));
-		var newcolor = ColorUtility.ToHtmlStringRGB(mycolor);
-		var stringContent = new StringContent(newcolor);
-		sendAPICall(stringContent);
-		
+		if (gameObject.name == "Switcher Variant") {
+			Debug.Log("Switchieeeeeeeeeeeeeeee");
+			if (aanUit == true)
+			{
+				aanUit = false;
+				var newcolor = "000000";
+				sendAPICall(newcolor);
+			}
+			else if (aanUit == false)
+			{
+				aanUit = true;
+				var newcolor = "FFFFFF";
+				sendAPICall(newcolor);
+			}
+		}
+		else
+		{
+			if (aanUit == true) { 
+			var mycolor = gameObject.transform.GetChild(1).gameObject.GetComponent<Renderer>().material.color;
+			Debug.Log("Hello:" + ColorUtility.ToHtmlStringRGB(mycolor));
+			var newcolor = ColorUtility.ToHtmlStringRGB(mycolor);
+			//var stringContent = JsonSerializer.Serialize(newcolor);
+			sendAPICall(newcolor);
+			}
+		}
 
 		if (buttonMeshObject != null)
 		{
@@ -71,10 +96,10 @@ public class ButtonLogic : MonoBehaviour {
 		MethodsToCallMouseUp.Invoke();
 
 	}
-	public async void sendAPICall(StringContent mycolor)
+	public async void sendAPICall(string mycolor)
 	{
-		HttpResponseMessage response = await client.PostAsync(
-				"http://192.168.168.168:5000/rgb/", mycolor);
+		HttpResponseMessage response = await client.GetAsync(
+				"http://192.168.168.168:5000/rgb/" + mycolor);
 		response.EnsureSuccessStatusCode();
 
 		// return URI of the created resource.
@@ -84,30 +109,35 @@ public class ButtonLogic : MonoBehaviour {
 	public void ButtonNoLongerPressed()
 	{
 		Debug.Log("Check");
-		if(buttonMeshObject != null)
+		if (buttonMeshObject != null)
 		{
 			StartCoroutine("AnimateButtonUp");
 		}
 	}
 
 
-	IEnumerator AnimateButtonDown() {
+	IEnumerator AnimateButtonDown()
+	{
 		StopCoroutine("AnimateButtonUp");
 		float travelled = 0;
 		buttonMeshObject.transform.localPosition = startPos;
-		while (travelled < buttonTravelDistance) {
+		while (travelled < buttonTravelDistance)
+		{
 			travelled += downTravelSpeed;
-			buttonMeshObject.transform.localPosition -= new Vector3(0,1,0) * downTravelSpeed;
+			buttonMeshObject.transform.localPosition -= new Vector3(0, 1, 0) * downTravelSpeed;
 			yield return null;
 		}
 	}
-	IEnumerator AnimateButtonUp() {
+	IEnumerator AnimateButtonUp()
+	{
 		float travelled = 0;
-		while (travelled < buttonTravelDistance) {
+		while (travelled < buttonTravelDistance)
+		{
 			travelled += upTravelSpeed;
-			buttonMeshObject.transform.localPosition += new Vector3(0,1,0) * upTravelSpeed;
+			buttonMeshObject.transform.localPosition += new Vector3(0, 1, 0) * upTravelSpeed;
 			yield return null;
 		}
 		buttonMeshObject.transform.localPosition = startPos;
 	}
+	
 }
---
+++ Assets/onoffScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onoffScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Switcher")
+                { print("My object is clicked by mouse"); }
+            }
+        }
+    }
+}
